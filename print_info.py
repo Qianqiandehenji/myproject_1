0d87e0676af9208b51dc580a787d2e53c69f9d80
Qianqiandehenji <liguangyin2012@gmail.com>
2024-02-20 14:58:44 +0800

    Transformed to functional!

--- print_info.py
+++ print_info.py
@@ -1,22 +1,15 @@
 {\rtf1\ansi\ansicpg1252\cocoartf2636
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\froman\fcharset0 Times-Roman;\f1\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;\red255\green255\blue255;\red1\green21\blue77;\red0\green0\blue0;
-}
-{\*\expandedcolortbl;;\cssrgb\c100000\c100000\c100000;\cssrgb\c0\c12549\c37647;\cssrgb\c0\c0\c0;
-}
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\froman\fcharset0 Times-Roman;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
+{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;}
 \paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
 \deftab720
 \pard\pardeftab720\partightenfactor0
 
-\f0\fs37\fsmilli18667 \cf2 \cb3 \expnd0\expndtw0\kerning0
-def print_info(arg):\
-    message = f"Hello \{arg\}"\
-    return message\
-\pard\pardeftab720\sa240\partightenfactor0
-\cf2 greet_person = print_info("Chris") print(greet_person) 
-\fs24 \cf4 \cb1 \
+\f0\fs24 \cf2 \expnd0\expndtw0\kerning0
+Def print_info(arg):\
+	message = f\'94Hello \{arg\}\'94\
+	return message\
 \
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
-
-\f1 \cf0 \kerning1\expnd0\expndtw0 \
-}
+greet_person = print_info(\'93QQ\'94)\
+print(greet_person)}
